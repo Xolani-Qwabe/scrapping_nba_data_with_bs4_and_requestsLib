ca588d44efb3385fd2933e0c00f31572b909c471
xolani <thabisoxolaniqwabe@gmail.com>
2024-02-13 12:56:43 +0200

    game stats sorted by player

---
+++ reading_cleaning_data/clean_box_scores/sort_frames_by_player.py
@@ -0,0 +1,48 @@
+import pandas as pd
+import os
+
+
+def update_dataframe(csv_file, dataframe_dict):
+    # Extract game details from file name
+    game_details = os.path.basename(csv_file).split('_')[3:-1]
+    game_details = ' '.join(game_details)
+
+    # Read the CSV file into a DataFrame
+    new_data = pd.read_csv(csv_file)
+
+    # Iterate over each row in the CSV file
+    for index, row in new_data.iterrows():
+        player_name = row['Starters']
+
+        # Check if the player's data already exists in the dictionary
+        if player_name in dataframe_dict:
+            # If the player's data exists, append the row to the existing DataFrame
+            row['GameDetails'] = game_details  # Add game details column
+            dataframe_dict[player_name] = pd.concat([dataframe_dict[player_name], row.to_frame().T], ignore_index=True)
+        else:
+            # If the player's data doesn't exist, create a new DataFrame for that player
+            player_df = pd.DataFrame(row).T
+            player_df['GameDetails'] = game_details  # Add game details column
+            dataframe_dict[player_name] = player_df
+
+    return dataframe_dict
+
+
+def main():
+    folder_path = './advanced_clean'
+
+    dataframe_dict = {}
+
+    # Iterate over each file in the folder
+    for file_name in os.listdir(folder_path):
+        if file_name.endswith('.csv'):
+            file_path = os.path.join(folder_path, file_name)
+            dataframe_dict = update_dataframe(file_path, dataframe_dict)
+
+    # Save each player's DataFrame to separate CSV files
+    for player_name, player_dataframe in dataframe_dict.items():
+        player_dataframe.to_csv(f'{player_name}_data.csv', index=False)
+
+
+if __name__ == "__main__":
+    main()
--- reading_cleaning_data/test_cleaner.py
+++ reading_cleaning_data/test_cleaner.py
@@ -1,14 +1,4 @@
-import os
 import pandas as pd
 
-
-def convert_to_minutes(time_str):
-    hours, minutes = map(int, time_str.split(':'))
-    return hours * 60 + minutes
-
-#
-#This will take minutes convert to int
-# basic['MP'] = basic['MP'].apply(convert_to_minutes)
-#
-#This will only grab the row where staters == Devin Vassell
-# basic[basic['Starters'] == 'Devin Vassell']
+df = pd.read_csv('./advanced_player_sorted_csv/A.J. Green_data.csv')
+print(df)
